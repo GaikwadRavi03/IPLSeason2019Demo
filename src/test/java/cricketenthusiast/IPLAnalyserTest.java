37431bece88813a86768a7c3174bb453484ab436
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-12-14 14:54:23 +0530

    Added test Case for IPL 2019 CSV File Delimiter Problem Should Return Exception.

--- src/test/java/cricketenthusiast/IPLAnalyserTest.java
+++ src/test/java/cricketenthusiast/IPLAnalyserTest.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 public class IPLAnalyserTest {
 
     private static final String RUNS_FACT_SHEET = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String DELIMITER_RUNS_FACT_SHEET = "./src/test/resources/Delimiter_IPL2019FactsheetMostRuns.csv";
     private static final String WICKETS_FACT_SHEET = ".src/test/resources/IPL2019FactsheetMostWkts.csv";
     private static final String WRONG_FACT_SHEET = "./src/test/resources/IPL2019FactsheetMostRuns12.csv";
 
@@ -26,10 +27,18 @@ public class IPLAnalyserTest {
         try {
             IPLAnalyser IPLAnalyser = new IPLAnalyser();
             long actual = IPLAnalyser.batsmanDetails(WRONG_FACT_SHEET);
-//            Assert.assertEquals(101, actual);
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals("Wrong File", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenIPL2019_CSVFileDelimiterProb_ShouldReturnException() {
+        try {
+            IPLAnalyser IPLAnalyser = new IPLAnalyser();
+            IPLAnalyser.batsmanDetails(DELIMITER_RUNS_FACT_SHEET);
         } catch (IPLAnalyserException e) {
             Assert.assertEquals(IPLAnalyserException.ExceptionType.FILE_PROBLEM, e.type);
-            e.printStackTrace();
         }
     }
 }
