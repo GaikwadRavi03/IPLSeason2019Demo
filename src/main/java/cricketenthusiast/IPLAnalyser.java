dae1c507fb3a059f58210c78a79cf34e8f55b201
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-12-16 11:17:31 +0530

    Refactored code for 6s and 4s with striking rate maximun hit best or worst player.

--- src/main/java/cricketenthusiast/IPLAnalyser.java
+++ src/main/java/cricketenthusiast/IPLAnalyser.java
@@ -23,6 +23,7 @@ public class IPLAnalyser {
         this.sortingMap.put(SortingFields.AVERAGE, Comparator.comparing(census -> census.average));
         this.sortingMap.put(SortingFields.STRIKE_RATE, Comparator.comparing(census -> census.strikeRate));
         this.sortingMap.put(SortingFields.SIX_AND_FOURS, new SortingFieldsComparator());
+        this.sortingMap.put(SortingFields.SIX_AND_FOURS, new SortingFieldsComparator().thenComparing(census -> census.strikeRate));
     }
 
     public long batsmanDetails(String filePath) throws IPLAnalyserException {
