cc54dd3c539945667158272879b02ac112575e8b
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-12-16 11:19:30 +0530

    Added TestCase for worst player not hit 6s and 4s with striking rate.

--- src/test/java/cricketenthusiast/IPLAnalyserTest.java
+++ src/test/java/cricketenthusiast/IPLAnalyserTest.java
@@ -129,4 +129,15 @@ public class IPLAnalyserTest {
         } catch (IPLAnalyserException e) {
         }
     }
+
+    @Test
+    public void givenMessage_SortedListOfRuns_ShouldReturnWorstBatsman_BySixesAndSixesWithStrikingRate() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.batsmanDetails(RUNS_FACT_SHEET);
+            List<IPLMostRunCsv> sortedIplData = iplAnalyser.sortedPlayersData(SortingFields.SIX_AND_FOURS);
+            Assert.assertEquals("Shakib Al Hasan", sortedIplData.get(100).player.trim());
+        } catch (IPLAnalyserException e) {
+        }
+    }
 }
